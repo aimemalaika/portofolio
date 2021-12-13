b7f3a01bd6c35ba833960dc2e9c83f9ff1e70c8b
Aime Malaika <aimemalaika1995@gmail.com>
2021-12-13 17:09:14 +0200

    Fix linter errors

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,25 @@
-const navigation = document.querySelector('.navigation')
-const header = document.querySelector('.toolbar')
+const navigation = document.querySelector('.navigation');
+const header = document.querySelector('.toolbar');
 const expandMenu = () => {
-    navigation.classList.remove('animate-left');
-    navigation.classList.add('animate-right')
+  navigation.classList.remove('animate-left');
+  navigation.classList.add('animate-right');
 }
 
 const closeMenu = () => {
-    navigation.classList.remove('animate-right')
-    navigation.classList.add('animate-left');
+  navigation.classList.remove('animate-right');
+  navigation.classList.add('animate-left');
 }
 
 document.addEventListener('scroll', function () {
-    if (window.scrollY > 0) {
-        if(!header.classList.contains('sticky')){
-            header.classList.add('sticky')
-        }
-    } else {
-        header.classList.remove('sticky')
+  if (window.scrollY > 0) {
+    if (!header.classList.contains('sticky')) {
+      header.classList.add('sticky');
     }
+  } else {
+    header.classList.remove('sticky');
+  }
 })
 
-document.querySelector('.open').addEventListener('click', expandMenu)
-let closeClass = document.querySelectorAll('.close')
-
-closeClass.forEach((element => element.addEventListener('click', closeMenu)))
+document.querySelector('.open').addEventListener('click', expandMenu);
+let closeClass = document.querySelectorAll('.close');
+closeClass.forEach((element => element.addEventListener('click', closeMenu)));

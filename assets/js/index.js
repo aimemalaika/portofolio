0a552fe4b919a0f3650a62a931ab4072b3d1859f
Aime Malaika <aimemalaika1995@gmail.com>
2021-12-14 08:47:18 +0200

    Add active status on current menu desktop

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,6 @@
 const navigation = document.querySelector('.navigation');
 const header = document.querySelector('.toolbar');
+const navItems = document.querySelectorAll('.nav-item');
 const expandMenu = () => {
   navigation.classList.remove('animate-left');
   navigation.classList.add('animate-right');
@@ -10,6 +11,16 @@ const closeMenu = () => {
   navigation.classList.add('animate-left');
 };
 
+const setActive = (target = null) => {
+  navItems.forEach((element) => {
+    if (element.getAttribute('href') === target) {
+      element.classList.add('active');
+    } else {
+      element.classList.remove('active');
+    }
+  });
+};
+
 document.addEventListener('scroll', () => {
   if (window.scrollY > 0) {
     if (!header.classList.contains('sticky')) {
@@ -17,6 +28,20 @@ document.addEventListener('scroll', () => {
     }
   } else {
     header.classList.remove('sticky');
+  }
+  switch (true) {
+    case window.scrollY >= 879.75 && window.scrollY < 2406.75:
+      setActive('#portofolio');
+      break;
+    case window.scrollY >= 2406.75 && window.scrollY < 3257.5:
+      setActive('#about');
+      break;
+    case window.scrollY >= 3257.5:
+      setActive('#contact-form');
+      break;
+    default:
+      setActive();
+      break;
   }
 });
 
